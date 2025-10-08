2eeb108aa3353bf86d52b6228f019f094747e5b6
Edward Ofori <oforikofi.edward@gmail.com>
2025-10-08 00:46:04 +0000

    First commit

---
+++ weather-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+    <h1 class="text-2xl font-bold relative size-70 ml-0 px-3 py-2 bg-[url() bg bg-local">Umbrella app</h1>
+    </div>
+  )
+}
+
+export default App
